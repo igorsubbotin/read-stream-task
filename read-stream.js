81cf5907378e4f2a623cd7af2a434ce1e4583919
Igor Subbotin <moat1981@gmail.com>
2017-06-29 01:04:06 +0300

    Implement functionality without continuosly adding and removing events of incoming stream

--- read-stream.js
+++ read-stream.js
@@ -1,28 +1,71 @@
-const events = require('events');
+const EventEmitter = require('events');
+const stateChangedEventName = 'stateChanged';
+
+class StreamWrapper extends EventEmitter {
+    constructor(stream) {
+        super();
+        this.stream = stream;
+        this.error = null;
+        this.ended = false;
+        this.data = null;
+
+        stream.on('data', (chunk) => {
+            this.data = chunk;
+            this.stream.pause();
+            this.emit(stateChangedEventName);
+        });
+        stream.on('end', () => {
+            this.ended = true;
+            this.emit(stateChangedEventName);
+        });
+        stream.on('error', (err) => {
+            this.error = err;
+            this.emit(stateChangedEventName);
+        });
+    }
+
+    readData() {
+        if (!this.data) {
+            return null;
+        }
+        const chunk = this.data;
+        this.data = null;
+        this.stream.resume();
+        return chunk;
+    }
+
+    createPromise() {
+        const wrapper = this;
+        return new Promise((resolve, reject) => {
+            const resolvePromise = () => {
+                const chunk = wrapper.readData();
+                if (chunk) {
+                    resolve(chunk);
+                    return true;
+                }
+                if (wrapper.error) {
+                    reject(wrapper.error);
+                    return true;
+                } 
+                if (wrapper.ended) {
+                    resolve(null);
+                    return true;
+                }
+                return false;
+            }
+
+            if (resolvePromise()) {
+                return;
+            }
+
+            wrapper.once(stateChangedEventName, () => {
+                resolvePromise();
+            });
+        });
+    }
+}
 
 module.exports = (stream) => {
-  return () => {
-    return new Promise((resolve, reject) => {
-        const removeListeners = () => {
-            stream.removeListener('data', onData);
-            stream.removeListener('end', onEnd);
-            stream.removeListener('error', onError);
-        }
-        const onData = (chunk) => {
-            removeListeners();
-            resolve(chunk);
-        }
-        stream.on('data', onData);
-        const onEnd = () => {
-            removeListeners();
-            resolve(false);
-        }
-        stream.on('end', onEnd);
-        const onError = (err) => {
-            removeListeners();
-            reject(err);
-        }
-        stream.on('error', onError);
-      });
-  };
+    const wrapper = new StreamWrapper(stream);
+    return () => { return wrapper.createPromise() };
 }
--- test/read-stream-tests.js
+++ test/read-stream-tests.js
@@ -3,6 +3,8 @@ const should = chai.should();
 const fs = require('fs');
 const readStream = require('../read-stream');
 
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 describe('Read stream tests', () => {
     it('reads small file correctly', async () => {
       // arrange
@@ -46,9 +48,9 @@ describe('Read stream tests', () => {
       i.should.be.equal(17); // should be calculated depends on file size actually
       fileContent = fs.readFileSync(path).toString();
       content.should.be.equal(fileContent);
-      stream.listenerCount('data').should.be.equal(0);
-      stream.listenerCount('end').should.be.equal(1);
-      stream.listenerCount('error').should.be.equal(0);
+      stream.listenerCount('data').should.be.equal(1);
+      stream.listenerCount('end').should.be.equal(2);
+      stream.listenerCount('error').should.be.equal(1);
     });
 
     it('handles error correctly', (done) => {
@@ -61,6 +63,32 @@ describe('Read stream tests', () => {
             err.code.should.be.equal('ENOENT');
             done();
         });
+    });
+
+    it('reads file correctly with delays', async () => {
+      // arrange
+      let path = 'data/large.txt';
+      let stream = fs.createReadStream(path, {highWaterMark: 200, encoding: 'utf-8'});
+      let data;
+      let reader = readStream(stream);
+
+      // act
+      let i = 0;
+      let size = 0;
+      let content = '';
+      while(data = await reader()) {
+        await sleep(100);
+        content += data;
+        i++;
+      }    
+
+      // assert
+      i.should.be.equal(6); // should be calculated depends on file size actually
+      fileContent = fs.readFileSync(path).toString();
+      content.should.be.equal(fileContent);
+      stream.listenerCount('data').should.be.equal(1);
+      stream.listenerCount('end').should.be.equal(2);
+      stream.listenerCount('error').should.be.equal(1);
     });
 });
 
